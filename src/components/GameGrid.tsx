1d80929be9da3dde3324ea43f7c93e9ca00275a5
Saurabh Singh <imsaurabhsingh007@gmail.com>
2023-09-20 21:12:22 +0530

    extract to query object

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,24 +1,17 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
-import { Platform, useGames } from '../hooks/useGames';
+import { useGames } from '../hooks/useGames';
 import { GameCard } from './GameCard';
 import { GameCardSkeleton } from './GameCardSkeleton';
 import { GameCardContainer } from './GameCardContainer';
-import { Genre } from '../hooks/useGenres';
+
+import { GameQuery } from '../App';
 
 interface GameGridProps {
-  selectedGenre: Genre | null;
-  selectedPlatform: Platform | null;
+  gameQuery: GameQuery;
 }
 
-export const GameGrid: React.FC<GameGridProps> = ({
-  selectedGenre,
-  selectedPlatform,
-}) => {
-  const {
-    data: games,
-    error,
-    isLoading,
-  } = useGames(selectedGenre, selectedPlatform);
+export const GameGrid: React.FC<GameGridProps> = ({ gameQuery }) => {
+  const { data: games, error, isLoading } = useGames(gameQuery);
 
   const skeletons = [1, 2, 3, 4, 5, 6];
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
+import { GameQuery } from '../App';
 import { useData } from './useData';
-import { Genre } from './useGenres';
 
 export interface Platform {
   id: number;
@@ -15,17 +15,14 @@ export interface Game {
   parent_platforms: { platform: Platform }[];
 }
 
-export const useGames = (
-  selectedGenre: Genre | null,
-  selectedPlatform: Platform | null
-) =>
+export const useGames = (gameQuery: GameQuery) =>
   useData<Game>(
     '/games',
     {
       params: {
-        genres: selectedGenre?.id,
-        platforms: selectedPlatform?.id,
+        genres: gameQuery.genre?.id,
+        platforms: gameQuery.platform?.id,
       },
     },
-    [selectedGenre?.id, selectedPlatform?.id]
+    [gameQuery]
   );
