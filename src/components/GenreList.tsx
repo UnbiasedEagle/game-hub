47010697542cdf89e873259c7f423e9382ebf0d0
Saurabh Singh <imsaurabhsingh007@gmail.com>
2023-09-21 21:58:14 +0530

    clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,11 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
   Spinner,
-  Text,
 } from '@chakra-ui/react';
 import { Genre, useGenres } from '../hooks/useGenres';
 import { getCroppedImageUrl } from '../services/image-url';
@@ -30,30 +30,38 @@ export const GenreList: React.FC<GenreListProps> = ({
   }
 
   return (
-    <List>
-      {genres.map((genre) => {
-        return (
-          <ListItem paddingY='5px' key={genre.id}>
-            <HStack>
-              <Image
-                src={getCroppedImageUrl(genre.image_background)}
-                boxSize='32px'
-                borderRadius={8}
-              />
-              <Button
-                fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
-                onClick={() => onSelectedGenre(genre)}
-                variant='link'
-                fontSize='lg'
-              >
-                <Text overflow='hidden' textOverflow='ellipsis'>
+    <>
+      <Heading fontSize='2xl' marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {genres.map((genre) => {
+          return (
+            <ListItem paddingY='5px' key={genre.id}>
+              <HStack>
+                <Image
+                  objectFit='cover'
+                  src={getCroppedImageUrl(genre.image_background)}
+                  boxSize='32px'
+                  borderRadius={8}
+                />
+                <Button
+                  whiteSpace='normal'
+                  textAlign='left'
+                  fontWeight={
+                    genre.id === selectedGenre?.id ? 'bold' : 'normal'
+                  }
+                  onClick={() => onSelectedGenre(genre)}
+                  variant='link'
+                  fontSize='lg'
+                >
                   {genre.name}
-                </Text>
-              </Button>
-            </HStack>
-          </ListItem>
-        );
-      })}
-    </List>
+                </Button>
+              </HStack>
+            </ListItem>
+          );
+        })}
+      </List>
+    </>
   );
 };
