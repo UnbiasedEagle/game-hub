cda05300ec1d6a58c0404a0bbba2ee7a787d1135
Saurabh Singh <imsaurabhsingh007@gmail.com>
2023-10-13 12:01:36 +0530

    move api key to .env

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '075dec536dbd462f92b7a9d05c5d8bb8',
+    key: import.meta.env.VITE_RAWG_API_KEY,
   },
 });
 
